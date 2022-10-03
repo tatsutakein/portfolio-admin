87a338988c8260eabc14fa6db4af285af76085ef
Ryo Takeuchi <develop@r09i.me>
2022-10-04 03:32:46 +0900

    :tada: first commit

---
+++ app/android/app/src/main/kotlin/com/ripomoea/portfolio_admin/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ripomoea.portfolio_admin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ app/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:portfolio_admin/src/utils/url_strategy/url_strategy.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+import 'src/app.dart';
+import 'src/settings/settings_controller.dart';
+import 'src/settings/settings_service.dart';
+
+void main() async {
+  // Set up the SettingsController, which will glue user settings to multiple
+  // Flutter Widgets.
+  final settingsController = SettingsController(SettingsService());
+
+  // Load the user's preferred theme while the splash screen is displayed.
+  // This prevents a sudden theme change when the app is first displayed.
+  await settingsController.loadSettings();
+
+  // NOTE: Webページで '#' が混在するのを防ぐ
+  usePathUrlStrategy();
+
+  await dotenv.load();
+
+  await Supabase.initialize(
+      url: dotenv.env['SUPABASE_URL'],
+      anonKey: dotenv.env['SUPABASE_ANON_KEY'],
+  );
+
+  // Run the app and pass in the SettingsController. The app listens to the
+  // SettingsController for changes, then passes it further down to the
+  // SettingsView.
+  runApp(MyApp(settingsController: settingsController));
+}
